9b022d413b70379bd70a9de1c7a0b067545fbfeb
Aashi858 <72612631+Aashi858@users.noreply.github.com>
2021-04-18 12:37:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-function preload(){}
+nose_x = 0;
+nose_y = 0;
+
+function preload(){
+    glasses = loadImage('https://i.postimg.cc/rs34qQ4Y/g.png');
+}
 function setup(){
     canvas = createCanvas(300,300);
     canvas.center();
@@ -11,6 +16,7 @@ function setup(){
 }
 function draw(){
     image(video,0,0,300,300);
+    image(glasses,nose_x,nose_y,100,30)
 }
 function takeSnapshot(){
     save("myFilter.png");
@@ -21,6 +27,8 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+        nose_x = results[0].pose.nose.x - 50;
+        nose_y = results[0].pose.nose.y - 40;
         console.log("Nose x position :" + results[0].pose.nose.x);
         console.log("Nose y position :" + results[0].pose.nose.y);
     }
